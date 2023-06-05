cf8af527793d93a89a4ff5290d6d9a93bc83a7c9
ajwo <caradornian.ajwo@gmail.com>
2023-06-05 02:06:36 +0200

    feature: CLI predict and train

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # structure-based-admet-prediction
 
-## Development
 ### Requirements
+
 * `conda` https://docs.conda.io/en/latest/
 
 ### Environment
+
 * Create new Conda environment `conda env create -f environment.yml `
 * Run `conda activate structure-based-admet-prediction`
-* Run `pip install -r requirements.txt`
+
+### Usage
+
+Supported CLI commands:
+
+* `dock` Ligand docking
+* `train-model` Train a (optionally supplied) regressor model to predict ADMET features
+* `predict` Predict ADMET features with supplied pipeline (trained with `train-model`)
+
+Run `python -m sbap --help` for commands usage details.
--- sbap/__main__.py
+++ sbap/__main__.py
@@ -1,13 +1,19 @@
 import logging
 import pathlib
+import pickle
 import shutil
 from datetime import datetime, timezone
-from typing import Optional
+from typing import Optional, cast
 
+import pandas as pd
+import rich
 import typer
+from sklearn.ensemble import RandomForestRegressor
 
 from sbap.docking import SminaConfig
-from sbap.featurizers.prolif_smina import SminaDockingPersistenceHandler
+from sbap.featurizers.prolif_smina import DockedProlifFingerprintFeaturizer, \
+    SminaDockingPersistenceHandler
+from sbap.pipeline import Regressor, Pipeline
 
 app = typer.Typer()
 
@@ -29,8 +35,44 @@ def dock(
         logging.info(f"Docking results will be saved to {docked_ligands_target_directory.absolute()}")
     shutil.rmtree(docked_ligands_target_directory, ignore_errors=True)
     config = SminaConfig.parse_file(smina_config_file, content_type="yaml")
-    handler = SminaDockingPersistenceHandler.create(config, str(docked_ligands_target_directory), logging.INFO)
+    handler = SminaDockingPersistenceHandler.create(config, str(docked_ligands_target_directory),
+                                                    logging.INFO)  # pass unlabeled persistence here
     handler.dock(protein_pdb_file, sdf_file, batch_size=batch_size, starting_batch=starting_batch)
+
+
+@app.command()
+def train_model(
+        sdf_file: pathlib.Path,
+        protein_pdb_file: pathlib.Path,
+        smina_config_file: pathlib.Path,
+        output_model_path: pathlib.Path,
+        pickled_regressor_path: Optional[pathlib.Path] = None,
+) -> None:
+    config = SminaConfig.parse_file(smina_config_file, content_type="yaml")
+    fingerprint_featurizer = DockedProlifFingerprintFeaturizer.create(
+        logging_level=logging.INFO,
+    )
+    if pickled_regressor_path is not None:
+        with open(pickled_regressor_path, 'rb') as f:
+            model: Regressor = pickle.load(f)
+    else:
+        model = cast(Regressor, RandomForestRegressor())
+    pipeline = Pipeline(config, fingerprint_featurizer, model)
+    rich.print(f"r2 score: {pipeline.fit(protein_pdb_file, sdf_file)}")
+    with open(output_model_path, "wb") as f:
+        pickle.dump(pipeline, f)
+
+
+@app.command()
+def predict(
+        sdf_file: pathlib.Path,
+        pickled_pipeline_path: pathlib.Path,
+        output_path: pathlib.Path,
+) -> None:
+    with open(pickled_pipeline_path, "rb") as f:
+        pipeline: Pipeline = pickle.load(f)
+    output = pipeline.predict(sdf_file)
+    pd.DataFrame(output).to_csv(output_path, header=False, index=False)
 
 
 app()
